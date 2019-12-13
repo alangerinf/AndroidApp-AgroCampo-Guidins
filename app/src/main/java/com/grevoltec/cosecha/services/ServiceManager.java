c12be6d4ee531b4cc57626890cd183edcd4359e7
Alan Gerónimo <contacto@alangeronimo.com>
2019-12-13 11:09:21 -0500

    cambio a guidins

--- app/src/main/java/com/grevoltec/cosecha/services/ServiceManager.java
+++ app/src/main/java/com/grevoltec/cosecha/services/ServiceManager.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ServiceManager {
 
     private static Retrofit retrofit = null;
-    private static final String BASE_URL = "http://apicosechacopacabana.ibao.pe/";
+    private static final String BASE_URL = "http://apicosechagiddings.ibao.pe/";
 
     public  static  Retrofit getRetrofit(){
         if (retrofit == null) {
